74a6025b3ce92973b3600aa5500d656204ba716d
Theres Östgård <theresb88@gmail.com>
2024-12-04 09:49:52 +0100

    Add project files.

---
+++ DBDay04/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBDay04
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public bool IsVaccinated { get; set; }
+        public int Age { get; set; }
+        public int AmountOfLegs { get; set; }
+
+        public string Species { get; set; }
+
+
+    }
+}
---
+++ DBDay04/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DBDay04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animalsInHospital = new List<Animal>()
+            {
+                new Animal { Name = "Lucas", IsVaccinated = true, Age = 5, AmountOfLegs = 4, Species = "Dog" },
+                new Animal { Name = "Björne", IsVaccinated = true, Age = 3, AmountOfLegs = 4, Species = "Dog" },
+                new Animal { Name = "Mira", IsVaccinated = false, Age = 4, AmountOfLegs = 4, Species = "Dog" },
+                new Animal { Name = "Zacco", IsVaccinated = true, Age = 7, AmountOfLegs = 4, Species = "Dog" },
+                new Animal { Name = "Pelle", IsVaccinated = true, Age = 2, AmountOfLegs = 4, Species = "Cat" },
+                new Animal { Name = "Lisa", IsVaccinated = true, Age = 4, AmountOfLegs = 4, Species = "Cat" },
+                new Animal { Name = "Tuva", IsVaccinated = false, Age = 13, AmountOfLegs = 4, Species = "Cat" },
+                new Animal { Name = "Sven", IsVaccinated = false, Age = 1, AmountOfLegs = 2, Species = "Parrot" },
+                new Animal { Name = "Beast", IsVaccinated = false, Age = 2, AmountOfLegs = 8, Species = "Spider" },
+                new Animal { Name = "Hoppe", IsVaccinated = true, Age = 3, AmountOfLegs = 4, Species = "Rabbit" },
+                new Animal { Name = "Shadow", IsVaccinated = false, Age = 5, AmountOfLegs = 0, Species = "Snake" }
+            };
+
+            var amountOfDogs = animalsInHospital.Where(d => d.Species == "Dog").Count();
+
+            var oldestAnimalinHospital = animalsInHospital.Max(d => d.Age);
+
+            var vaccinatedAnimals = animalsInHospital.Where(a => a.IsVaccinated == true).ToList();
+
+            var fourLeggedAnimalsOver3 = animalsInHospital.Where(f => f.AmountOfLegs == 4 && f.Age > 3).ToList();
+
+            var isThereAhadowAnimal = animalsInHospital.Where(n => n.Name == "Shadow");
+        }
+    }
+}
